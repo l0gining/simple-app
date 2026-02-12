c36d9d9a568f6a51fff0a92dbfdf080ce42686a6
l0gining <logining15678@gmail.com>
2026-02-12 10:11:24 +0000

    Add secrets to scan

--- config.py
+++ config.py
@@ -1,10 +1,6 @@
 aws_access_key_id = "AKIAIOSFODNN7EXAMPLE"
 aws_secret_access_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
 
-slack_webhook = "https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX"
-
-api_secret = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
-
 jwt_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
 
 rsa_private_key_header = "-----BEGIN RSA PRIVATE KEY-----"
