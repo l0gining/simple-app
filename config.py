202a5302ff263de0bcc54654f7532547bc702506
l0gining <logining15678@gmail.com>
2026-02-12 10:19:07 +0000

    Removed secrets, trying to scan

--- config.py
+++
@@ -1,6 +0,0 @@
-aws_access_key_id = "AKIAIOSFODNN7EXAMPLE"
-aws_secret_access_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
-
-jwt_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
-
-rsa_private_key_header = "-----BEGIN RSA PRIVATE KEY-----"
