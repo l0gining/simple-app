aa037f29e26fa667faf79df55780e1ffd26fd013
l0gining <logining15678@gmail.com>
2026-02-12 11:50:00 +0000

    SAST

--- vuln_app.py
+++ vuln_app.py
@@ -1,14 +1,34 @@
+"""
+Intentionally vulnerable code for SAST testing
+"""
 import os
 import pickle
+import subprocess
 
 def unsafe_deserialize(data):
     return pickle.loads(data)
 
-def sql_injection_example(user_input):
 
-    query = "SELECT * FROM users WHERE username = '" + user_input + "'"
+def get_user_data(username):
+    query = "SELECT * FROM users WHERE username = '" + username + "'"
     return query
 
-def command_injection(filename):
+def read_file(filename):
     os.system("cat " + filename)
+
+def run_command(cmd):
+    subprocess.call(cmd, shell=True)
+
+password = "admin123"
+api_token = "secret_token_12345"
+
+import hashlib
+def hash_password(pwd):
+    return hashlib.md5(pwd.encode()).hexdigest()
+
+def calculate(expression):
+    return eval(expression)
+
+def validate_user(user):
+    assert user.is_admin, "Not an admin"
 
