d66bd34d7665bd793185c09821cfcd5232d31656
l0gining <logining15678@gmail.com>
2026-02-12 10:05:16 +0000

    Add secrets to scan

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
 #fake secrets
-API_KEY = "abc-1234567890abcdef"
-DB_URL = "postgresql://admin:password123@localhost:5432/mydb"
-SOME_SECRET = "sdfgojerwtgdfs/sd3odsjgmd"
+AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+GITHUB_TOKEN = "ghp_1234567890abcdefghijklmnopqrstuvwxyzAB"
+API_KEY = "AKIAIOSFODNNexampleABCDEFGHIJKLMNOP"
+DATABASE_PASSWORD = "SuperSecretPassword123!"
+DB_CONNECTION = "postgresql://admin:SuperSecretPass123@db.example.com:5432/prod"
+PRIVATE_KEY = """-----BEGIN RSA PRIVATE KEY-----
+MIIEpAIBAAKCAQEA1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKL
+-----END RSA PRIVATE KEY-----"""
